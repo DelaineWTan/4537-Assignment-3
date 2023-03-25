7502bc951b14c3806bf3be27d041751a86016797
Delaine Tan <delainewtan@gmail.com>
2023-03-24 19:52:40 -0700

    Clean up Pagination.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import Pagination from "./Pagination";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
 function App() {
   const PAGE_SIZE = 10;
-  const [pokemons, setPokemon] = useState([])
+  const [pokemons, setPokemon] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   useEffect(() => {
     async function fetchData() {
-      const result = await axios.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json")
+      const result = await axios.get(
+        "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+      );
       setPokemon(result.data);
     }
-    fetchData()
-    
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      <Page pokemons={pokemons} PAGE_SIZE={PAGE_SIZE} currentPage={currentPage}/>
-      <Pagination/>
+      <Page
+        pokemons={pokemons}
+        PAGE_SIZE={PAGE_SIZE}
+        currentPage={currentPage}
+      />
+      <Pagination
+        pokemons={pokemons}
+        PAGE_SIZE={PAGE_SIZE}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 }
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
 
-function Pagination() {
-    return (
-        <div>Pagination</div>
-    )
+function Pagination({ pokemons, PAGE_SIZE, currentPage, setCurrentPage }) {
+
+  return (
+    <div>
+      <button disabled={currentPage < 2} onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
+      {Array.from(
+        { length: Math.ceil(pokemons.length / PAGE_SIZE) },
+        (element, index) => index
+      ).map((number) => (
+        <button key={number + 1} className={currentPage === number + 1 ? "buttonActive" : ""} onClick={() => setCurrentPage(number + 1)}>
+          {number + 1}
+        </button>
+      ))}
+      <button disabled={currentPage > Math.floor(pokemons.length / PAGE_SIZE)} onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+    </div>
+  );
 }
 
-export default Pagination
+export default Pagination;
