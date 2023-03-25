751d10c803a0faead278dc44c4d2d68af7023b2b
Delaine Tan <delainewtan@gmail.com>
2023-03-24 20:15:47 -0700

    Address additional requirements (responsive images in a row, pagination limitation).

--- src/Page.js
+++ src/Page.js
@@ -1,17 +1,22 @@
 import React from "react";
 
 function Page({ pokemons, PAGE_SIZE, currentPage }) {
-    // slice pokemon array based on current page and page size
-    const startIndex = (currentPage - 1) * PAGE_SIZE
-    const endIndex = startIndex + PAGE_SIZE
-    pokemons = pokemons.slice(startIndex, endIndex)
+  // slice pokemon array based on current page and page size
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  pokemons = pokemons.slice(startIndex, endIndex);
   return (
-    <div>
-      {pokemons.map((pokemon) => (
-        <div key={pokemon.id}>
-          <h1>{pokemon.name.english}</h1>
-        </div>
-      ))}
+    <div className="image-container">
+      {pokemons.map((pokemon) => {
+        const pokemonId = String(pokemon.id).padStart(3, "0");
+        return (
+          <img
+            className="image"
+            alt={pokemon.name.english}
+            src={`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokemonId}.png`}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/Pagination.js
+++ src/Pagination.js
@@ -2,19 +2,37 @@ import React from "react";
 import "./style.css";
 
 function Pagination({ pokemons, PAGE_SIZE, currentPage, setCurrentPage }) {
-
+  const pageNumbers = [];
+  for (
+    let i = Math.max(0, currentPage - 5);
+    i <= Math.min(Math.floor(pokemons.length / PAGE_SIZE), currentPage + 5);
+    i++
+  ) {
+    pageNumbers.push(i);
+  }
   return (
     <div>
-      <button disabled={currentPage < 2} onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
-      {Array.from(
-        { length: Math.ceil(pokemons.length / PAGE_SIZE) },
-        (element, index) => index
-      ).map((number) => (
-        <button key={number + 1} className={currentPage === number + 1 ? "buttonActive" : ""} onClick={() => setCurrentPage(number + 1)}>
-          {number + 1}
+      <button
+        disabled={currentPage < 2}
+        onClick={() => setCurrentPage(currentPage - 1)}
+      >
+        Previous
+      </button>
+      {pageNumbers.map((pageNumber) => (
+        <button
+          key={pageNumber + 1}
+          className={currentPage === pageNumber + 1 ? "buttonActive" : ""}
+          onClick={() => setCurrentPage(pageNumber + 1)}
+        >
+          {pageNumber + 1}
         </button>
       ))}
-      <button disabled={currentPage > Math.floor(pokemons.length / PAGE_SIZE)} onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+      <button
+        disabled={currentPage > Math.floor(pokemons.length / PAGE_SIZE)}
+        onClick={() => setCurrentPage(currentPage + 1)}
+      >
+        Next
+      </button>
     </div>
   );
 }
