f66db824ab14716d8e9dba680c41afa849b376d9
Delaine Tan <delainewtan@gmail.com>
2023-04-07 12:00:00 -0700

    Refresh access token on expiry.

--- src/Authenticated.js
+++ src/Authenticated.js
@@ -1,14 +1,20 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-const Authenticated = ({ children, setIsLoggedIn, accessToken }) => {
+const Authenticated = ({
+  children,
+  setIsLoggedIn,
+  accessToken,
+  setAccessToken,
+  refreshToken,
+}) => {
   const serverUrl = process.env.REACT_APP_SERVER_URL;
 
   useEffect(() => {
     // Function to check if the user is authenticated on the server
     const checkAuthentication = async () => {
       try {
-        console.log(accessToken)
+        console.log(accessToken);
         // Send a request to your API to check for the JWT token
         const response = await axios.get(`${serverUrl}/`, {
           headers: {
@@ -16,26 +22,43 @@ const Authenticated = ({ children, setIsLoggedIn, accessToken }) => {
           },
         });
         if (response.status !== 200) {
-            throw new Error(response.data.message); 
+          throw new Error(response.data.message);
         }
       } catch (error) {
-        setIsLoggedIn(false);
-        localStorage.removeItem('isLoggedIn');
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
-        localStorage.removeItem("user");
         console.error("Error checking authentication:", error);
+        // Refresh access token with refresh token
+        try {
+          const refreshResponse = await axios.post(
+            `${serverUrl}/requestNewAccessToken`,
+            {},
+            {
+              headers: {
+                Authorization: refreshToken, // Pass the refresh token in the authorization header
+              },
+            }
+          );
+          if (refreshResponse.status !== 200) {
+            throw new Error(refreshResponse.data.message);
+          }
+          const newAccessToken = refreshResponse.headers["auth-token-access"];
+          setAccessToken(newAccessToken);
+          localStorage.setItem("accessToken", newAccessToken);
+        } catch (refreshError) {
+          // Handle refresh token error
+          console.error("Error refreshing access token:", refreshError);
+          setIsLoggedIn(false);
+          localStorage.removeItem("isLoggedIn");
+          localStorage.removeItem("accessToken");
+          localStorage.removeItem("refreshToken");
+          localStorage.removeItem("user");
+        }
       }
     };
 
     checkAuthentication();
   }, [accessToken, serverUrl, setIsLoggedIn]);
 
-  return (
-    <div>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default Authenticated;
